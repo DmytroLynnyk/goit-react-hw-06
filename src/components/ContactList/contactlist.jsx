707c6ac964d1786a3f7221813cfcc2210a13c757
Dmytro Lynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-02-23 14:27:47 +0200

    Add files via upload

---
+++ src/components/ContactList/contactlist.jsx
@@ -0,0 +1,14 @@
+import { Contact } from '../Contact/contact';
+import css from './contactlist.module.css';
+
+export const ContactList = ({ users, deletUser }) => {
+  return (
+    <ul className={css.contactList}>
+      {users.map(user => (
+        <li key={user.id} className={css.contactItem}>
+          <Contact user={user} deletUser={deletUser} />
+        </li>
+      ))}
+    </ul>
+  );
+};
