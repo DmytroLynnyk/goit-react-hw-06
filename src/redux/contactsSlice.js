41fd337bbcf07d52bfefc0639af01d344f39d30c
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-02-24 16:35:07 +0200

    add persistor and localStore

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,19 +1,11 @@
-import {
-  createAction,
-  createReducer,
-  createSlice,
-  nanoid,
-} from '@reduxjs/toolkit';
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const contactsSlice = createSlice({
   name: 'user',
   initialState: {
-    items: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    items: [],
   },
 
   reducers: {
@@ -40,4 +32,14 @@ const contactsSlice = createSlice({
 });
 
 export const { addUser, deleteUser } = contactsSlice.actions;
-export const contactsReducer = contactsSlice.reducer;
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['items'],
+};
+
+export const contactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
