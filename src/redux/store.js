372e7d468481dec79ac56f32606e92094f92aeba
DmytroLynnyk <138771794+DmytroLynnyk@users.noreply.github.com>
2024-02-24 11:54:57 +0200

    Add combineReducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import { devToolsEnhancer } from '@redux-devtools/extension';
 import { nanoid } from '@reduxjs/toolkit';
 
@@ -28,52 +28,100 @@ export const changeFilter = newFilter => {
   };
 };
 
-const initialState = {
-  contacts: {
-    items: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-  },
-  filters: {
-    name: '',
-  },
+// const initialState = {
+//   contacts: {
+//     items: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//   },
+//   filters: {
+//     name: '',
+//   },
+// };
+const contactsInitialState = {
+  items: [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ],
 };
-const rootReducer = (state = initialState, action) => {
+const filtersInitialState = {
+  name: '',
+};
+
+const contactsReducer = (state = contactsInitialState, action) => {
   switch (action.type) {
     case 'user/addUser':
       return {
         ...state,
-        contacts: {
-          items: [...state.contacts.items, action.payload],
-        },
+        items: [...state.items, action.payload],
       };
 
     case 'user/deleteUser':
       return {
         ...state,
-        contacts: {
-          items: [
-            ...state.contacts.items.filter(
-              item => item.id !== action.payload.id
-            ),
-          ],
-        },
-      };
-
-    case 'filters/changeFilter':
-      return {
-        ...state,
-        filters: {
-          name: action.payload,
-        },
+        items: [...state.items.filter(item => item.id !== action.payload.id)],
       };
 
     default:
       return state;
   }
 };
+
+const filtersReducer = (state = filtersInitialState, action) => {
+  switch (action.type) {
+    case 'filters/changeFilter':
+      return {
+        ...state,
+        name: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
+});
+
+// const rootReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'user/addUser':
+//       return {
+//         ...state,
+//         contacts: {
+//           items: [...state.contacts.items, action.payload],
+//         },
+//       };
+
+//     case 'user/deleteUser':
+//       return {
+//         ...state,
+//         contacts: {
+//           items: [
+//             ...state.contacts.items.filter(
+//               item => item.id !== action.payload.id
+//             ),
+//           ],
+//         },
+//       };
+
+//     case 'filters/changeFilter':
+//       return {
+//         ...state,
+//         filters: {
+//           name: action.payload,
+//         },
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
 
 export const store = createStore(rootReducer, devToolsEnhancer());
